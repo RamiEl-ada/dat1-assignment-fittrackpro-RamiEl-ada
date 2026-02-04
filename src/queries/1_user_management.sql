4d4bc46de067a0b56c8024bb46926bc13f879b53
Rami-Ada <rami.elberdai-nasihi@ada.ac.uk>
2026-02-04 10:39:02 +0000

    queries 1 done

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -2,19 +2,59 @@
 .mode column
 
 -- 1.1
-
+SELECT 
+    member_id,
+    first_name,
+    last_name,
+    email,
+    join_date
+FROM members;
 
 -- 1.2
-
+UPDATE members
+SET email = 'emily.jones.updated@email.com', 
+    phone_number = '07000 100005'
+WHERE member_id = 5;
 
 -- 1.3
-
+SELECT COUNT(DISTINCT(member_id)) as member_count
+FROM members;
 
 -- 1.4
-
+SELECT 
+    m.member_id,
+    m.first_name,
+    m.last_name,
+    COUNT(ca.attendance_status) as registration_count
+FROM members as m
+INNER JOIN class_attendance as ca
+    ON m.member_id = ca.member_id
+    WHERE ca.attendance_status = 'Registered'
+GROUP BY 
+    m.member_id
+ORDER BY 
+    registration_count DESC
+LIMIT 1;
 
 -- 1.5
-
+SELECT 
+    m.member_id,
+    m.first_name,
+    m.last_name,
+    COUNT(ca.attendance_status) as registration_count
+FROM members as m
+INNER JOIN class_attendance as ca
+    ON m.member_id = ca.member_id
+WHERE ca.attendance_status = 'Registered'
+GROUP BY 
+    m.member_id
+ORDER BY 
+    registration_count
+LIMIT 1;
 
 -- 1.6
-
+SELECT 
+    COUNT(DISTINCT(attendance_status)) as Count
+FROM class_attendance 
+WHERE attendance_status = 'Attended'
+HAVING Count <= 2;
