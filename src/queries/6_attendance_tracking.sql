a31552fea37ad0abdab1e3ab7afa62ec03760ec1
Rami-Ada <rami.elberdai-nasihi@ada.ac.uk>
2026-02-11 11:10:48 +0000

    only 8.1 and 6.4 remain

--- src/queries/6_attendance_tracking.sql
+++ src/queries/6_attendance_tracking.sql
@@ -9,10 +9,24 @@ SELECT
     STRFTIME('%Y-%m-%d', check_in_time) as visit_date,
     check_in_time,
     check_out_time
-FROM attendance
+FROM attendance;
 
 -- 6.3 
-
+SELECT
+CASE strftime('%w', check_in_time)
+    WHEN '0' THEN 'Sunday'
+    WHEN '1' THEN 'Monday'
+    WHEN '2' THEN 'Tuesday'
+    WHEN '3' THEN 'Wednesday'
+    WHEN '4' THEN 'Thursday'
+    WHEN '5' THEN 'Friday'
+    WHEN '6' THEN 'Saturday'
+    END as day_of_week,
+    COUNT(member_id) AS visit_count
+FROM attendance
+GROUP BY day_of_week
+ORDER BY visit_count DESC
+LIMIT 1;
 
 -- 6.4 
 
--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -2,7 +2,21 @@
 .mode column
 
 -- 7.1 
-
+SELECT
+    staff_id,
+    first_name,
+    last_name,
+    position as role
+FROM staff;
 
 -- 7.2 
+SELECT
+    s.staff_id as trainer_id,
+    CONCAT(s.first_name, ' ', s.last_name ) as trainer_name,
+    COUNT(pts.session_id) as session_count
+FROM staff as s
+INNER JOIN personal_training_sessions as pts
+ON s.staff_id = pts.staff_id
+WHERE session_date BETWEEN '2025-01-20' AND '2025-02-19'
+
 
